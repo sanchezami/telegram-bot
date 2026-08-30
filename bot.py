6843b4fca2151bdaca3a2f4178baf73097abff30
sanchezami <gorelik_tema@mail.ru>
2026-08-30 21:54:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 # =========================================================
 
 TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0") or 0)
+ADMIN_ID = os.getenv("ADMIN_ID", "")
 
 DB = "bot.db"
 
